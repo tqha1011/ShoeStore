0a8ada5245a5ca435462f14b0f051ffacad46dce
Tran Quang Ha <tranquangha2006@gmail.com>
2026-03-04 11:42:38 +0700

    add scalar to test api

--- Backend/src/ShoeStore.Application/DependencyInjection/AddApplicationServices.cs
+++ Backend/src/ShoeStore.Application/DependencyInjection/AddApplicationServices.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using FluentValidation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +11,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration configuration)
     {
         // register dependency injection in application layer
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services;
     }
 }
