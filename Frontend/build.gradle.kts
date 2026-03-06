af74eaf775a287ec9f0705a12cd6b52464f11c01
Tran Quang Ha <tranquangha2006@gmail.com>
2026-03-06 18:34:19 +0700

    feat: setup prj in frontend

---
+++ Frontend/build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.sonarqube)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "tqha1011_ShoeStore")
+        property("sonar.organization", "tqha1011")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
