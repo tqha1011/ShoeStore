3525bbe0f78ca85b34783e1eab02a861e569e0af
Tran Quang Ha <tranquangha2006@gmail.com>
2026-03-11 16:09:40 +0700

    feat: register GenericRepository && UnitOfWork for DI

--- Backend/src/ShoeStore.Domain/Common/Entity.cs
+++ Backend/src/ShoeStore.Domain/Common/Entity.cs
@@ -2,5 +2,6 @@ namespace ShoeStore.Domain.Common;
 
 public abstract class Entity<TEntityId>(TEntityId id)
 {
+    // set id in constructor and make it init only to prevent changes after creation
     public TEntityId Id { get; init; } = id;
 }
