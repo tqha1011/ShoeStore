1e922e9f424e6766fc51e61eaf507205d63ea81e
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-03-20 01:54:25 +0700

    feat: Add methonds: add, get_all, get_by_id, update, delete

--- Backend/src/ShoeStore.Application/Interface/IGenericService.cs
+++ Backend/src/ShoeStore.Application/Interface/IGenericService.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ShoeStore.Application.Interface
+﻿namespace ShoeStore.Application.Interface
 {
-    internal interface IGenericService<TEntity, TEntityId>
-    where TEntity : Entity<TEntityId>
+    internal interface IGenericService<TEntity> where TEntity : class 
     {
-
+        // Create
+        Task<TEntity> AddAsync(TEntity entity);
+        // Read
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(Guid id);
+        // Update
+        Task UpdateAsync(TEntity entity);
+        // Delete
+        Task DeleteAsync(Guid id);
     }
 }
