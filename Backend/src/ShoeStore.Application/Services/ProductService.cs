c1c73b50fd5328a41d49e97c296b7158cceaefd7
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-03-20 02:23:50 +0700

    feat(product): Add add_product, get_id_product, update, delete

--- Backend/src/ShoeStore.Application/Services/ProductService.cs
+++ Backend/src/ShoeStore.Application/Services/ProductService.cs
@@ -5,9 +5,37 @@ using ShoeStore.Application.Interface;
 
 namespace ShoeStore.Application.Services
 {
-    internal class ProductService
+    internal class ProductService<Product> : IGenericRepository<Product>
     {
+        private readonly IUnitOfWork _uow;
+        private readonly IGenericRepository<Product> _productRepository;
+        public ProductService(IUnitOfWork uow, IGenericRepository<Product> productRepository)
+        {
+            _uow = uow;
+            _productRepository = productRepository;
+        }
 
+        public async Task<Product> AddASync(Product product)
+        {
+            _productRepository.AddAsync(product);
+            await _uow.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetByIdAsync(Guild id)
+        {
+            return await _productRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            return await _productRepository.Update(product);
+        }
+
+        public async Task<Product> DeleteAsync(Product product)
+        {
+            return await _productRepository.Delete(product);
+        }
 
     }
 }
