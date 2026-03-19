615cfefaf5c82b683332c3e0c1e7de096abde799
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-03-20 00:33:31 +0700

    feat: add ProductService.cs and IGenericService.cs

---
+++ Backend/src/ShoeStore.Application/Interface/IGenericService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoeStore.Application.Interface
+{
+    internal interface IGenericService<TEntity, TEntityId>
+    where TEntity : Entity<TEntityId>
+    {
+
+    }
+}
