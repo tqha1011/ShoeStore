966d664e0cd3d809e78aad4ca65a1d714207ba68
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-03-21 23:36:30 +0700

    fix(product): dont using async for add, update, delete

---
+++ Backend/src/ShoeStore.Application/Interface/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoeStore.Domain.Entities;
+
+namespace ShoeStore.Application.Interface
+{
+    internal interface IProductService
+    {
+        // Create
+        void AddProduct(Product product);
+        // Delete
+        void Delete(Product product);
+        // Update
+        void UpdateProduct(Product product);
+        // Get by id
+        Task<Product?> GetByIdAsyc(Guid productId);
+    }
+}
