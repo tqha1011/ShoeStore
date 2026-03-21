dc84544701b9a3889e30fcbed73306dd5a753714
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-03-21 23:46:58 +0700

    feat(product): Insert Add, Update, Delete, GetByIdAsync

--- Backend/src/ShoeStore.Application/Interface/IProductRepository.cs
+++ Backend/src/ShoeStore.Application/Interface/IProductRepository.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ShoeStore.Domain.Common;
+using ShoeStore.Domain.Entities; 
 
 namespace ShoeStore.Application.Interface
 {
     internal interface IProductRepository
     {
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        Task<Product?> GetByIdAsync(Guid id);
     }
 }
