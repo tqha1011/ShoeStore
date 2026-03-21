0894b06eb45db55f42873505549ed7441f312f3e
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-03-21 14:22:06 +0700

    chore: change the summary the controller above the namespavce

---
+++ Backend/src/ShoeStore.Api/Controllers/ProductAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoeStore.Api.Controllers
+{
+    public class ProductAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
