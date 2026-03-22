661cf17d88cba61e4636e883cfa8fc2be2756333
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-03-22 14:20:51 +0700

    featch(product user): search the product

--- Backend/src/ShoeStore.Api/Controllers/ProductController.cs
+++ Backend/src/ShoeStore.Api/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ShoeStore.Application.Interface;
+using ShoeStore.Application.DTOs.ProductDTOs;
 
 namespace ShoeStore.Api.Controllers
 {
@@ -9,6 +10,15 @@ namespace ShoeStore.Api.Controllers
     public class ProductsController(IProductService productService) : ControllerBase
     {
         [HttpGet("search")]
-        public async Task<IActionResult> Search()
+        public async Task<IActionResult> Search([FromQuery] ProductSearchRequest request)
+        {
+            var results = await productService.GetProductAsync(request);
+
+            if(results == null || !results.Any())
+            {
+                return NotFound(new { message = "Sorry we could not find that product" });
+            }
+            return Ok(results);
+        }
     }
 }
