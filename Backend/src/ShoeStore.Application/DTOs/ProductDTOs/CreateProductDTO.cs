5d7ed5eb94c6c87cab7ae7e72fa838ed8a9f7385
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-03-24 00:40:46 +0700

    fix

--- Backend/src/ShoeStore.Application/DTOs/ProductDTOs/CreateProductDTO.cs
+++ Backend/src/ShoeStore.Application/DTOs/ProductDTOs/CreateProductDTO.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using ShoeStore.Application.DTOs.ProducVariantDTOs;
-
+using ShoeStore.Application.DTOs.ProductVariantDTOs;
 namespace ShoeStore.Application.DTOs.ProductDTOs
 {
     public class CreateProductDto : ProductBaseDto
