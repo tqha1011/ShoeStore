19e21c8a5ce8d0547923c8ab9836407a13fbdcae
Tran Quang Ha <tranquangha2006@gmail.com>
2026-03-23 20:41:29 +0700

    Feature/restore pass (#97)
    * fix: get jwtkey from environment
    
    * feature(restore-password): complete feature restore password (send OTP to email,update password)
    
    * chore: fix typo in UpdatePasswordDto and add check Email valid
    
    * chore: add index to ProductSize

---
+++ Backend/src/ShoeStore.Infrastructure/Migrations/20260323130127_AddIndexProductPrice.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoeStore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexProductPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_product_variants_is_deleted_is_selling",
+                table: "product_variants");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_product_variants_is_deleted_is_selling_price",
+                table: "product_variants",
+                columns: new[] { "is_deleted", "is_selling", "price" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_product_variants_is_deleted_is_selling_price",
+                table: "product_variants");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_product_variants_is_deleted_is_selling",
+                table: "product_variants",
+                columns: new[] { "is_deleted", "is_selling" });
+        }
+    }
+}
