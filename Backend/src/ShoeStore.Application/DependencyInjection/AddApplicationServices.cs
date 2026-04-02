014d73531e060f4510df2c399a7af18e6172f145
Tran Quang Ha <tranquangha2006@gmail.com>
2026-04-02 14:29:39 +0700

    register DI for CartItemService

--- Backend/src/ShoeStore.Application/DependencyInjection/AddApplicationServices.cs
+++ Backend/src/ShoeStore.Application/DependencyInjection/AddApplicationServices.cs
@@ -3,6 +3,7 @@ using FluentValidation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ShoeStore.Application.Interface.Authentication;
+using ShoeStore.Application.Interface.CartItemInterface;
 using ShoeStore.Application.Interface.ProductInterface;
 using ShoeStore.Application.Services;
 
@@ -18,6 +19,7 @@ public static class DependencyInjection
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<IProductService, ProductService>();
         services.AddScoped<IProductVariantService, ProductVariantService>();
+        services.AddScoped<ICartItemService, CartItemService>();
         return services;
     }
 }
