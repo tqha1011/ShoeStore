18307cb23e216db73aa08ddc838393d3fcc4f729
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-04-07 23:17:06 +0700

    adding invoice detail response dto

---
+++ Backend/src/ShoeStore.Application/DTOs/InvoiceDetailDTOs/InvoiceDetailResponseDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ShoeStore.Application.DTOs.InvoiceDetailDTOs
+{
+    public class InvoiceDetailResponseDto
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public int Size { get; set; } = 0;
+        public string Color { get; set; } = string.Empty;
+        public int Quantity { get; set; } = 0;
+        public decimal UnitPrice { get; set; } = 0;
+
+    }
+}
