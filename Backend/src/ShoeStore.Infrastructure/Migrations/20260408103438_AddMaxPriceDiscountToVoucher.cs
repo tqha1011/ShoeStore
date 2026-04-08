63ac3f8cd7d054f3dadc25484fbe10d3efb0e78f
Tran Quang Ha <tranquangha2006@gmail.com>
2026-04-08 17:51:30 +0700

    config(Program.cs): configure CORS and add the endpoint for hub

---
+++ Backend/src/ShoeStore.Infrastructure/Migrations/20260408103438_AddMaxPriceDiscountToVoucher.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoeStore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMaxPriceDiscountToVoucher : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "max_price_discount",
+                table: "vouchers",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "max_price_discount",
+                table: "vouchers");
+        }
+    }
+}
