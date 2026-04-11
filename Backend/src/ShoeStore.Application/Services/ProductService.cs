36cdb9c6e8893f169b3fe2d50e34326a17077e4f
Tran Quang Ha <tranquangha2006@gmail.com>
2026-04-11 13:11:35 +0700

    perf(ProductService): integreate HybridCache

--- Backend/src/ShoeStore.Application/Services/ProductService.cs
+++ Backend/src/ShoeStore.Application/Services/ProductService.cs
@@ -5,6 +5,7 @@ using ShoeStore.Application.Constants;
 using ShoeStore.Application.DTOs;
 using ShoeStore.Application.DTOs.ProductDTOs;
 using ShoeStore.Application.DTOs.ProductVariantDTOs;
+using ShoeStore.Application.Extensions;
 using ShoeStore.Application.Interface.Common;
 using ShoeStore.Application.Interface.ProductInterface;
 using ShoeStore.Domain.Entities;
@@ -20,51 +21,60 @@ public class ProductService(IUnitOfWork uow, IProductRepository productRepositor
         if (request.PageIndex <= 0)
             return Error.Validation("Pagination.PageIndex", "Page index must be greater than 0.");
 
-        if (request.PageSize <= 0 || request.PageSize > 50)
+        if (request.PageSize is <= 0 or > 50)
             return Error.Validation("Pagination.PageSize", "Page size must be between 1 and 50.");
 
 
         // Get the queryable result from repository
         var query = productRepository.SearchProduct(request);
 
-        // Get total count before applying paging
-        var totalCount = await query.CountAsync(token);
-
         // Apply pagination and fetch products
-        var products = await query
-            .Select(p => new ProductResponseDto
+        var cachedListProduct = await cache.GetOrCreateAsync(
+            CacheKey.GenerateProductListCacheKey(request),
+            async cancel =>
             {
-                PublicId = p.PublicId,
-                Brand = p.Brand ?? string.Empty,
-                ProductName = p.ProductName,
-                Variants = p.ProductVariants
-                    .Where(v => v.IsSelling && !v.IsDeleted)
-                    .Select(v => new ProductVariantResponseDto
+                // Get total count before applying paging
+                var totalCount = await query.CountAsync(cancel);
+                
+                var products = await query
+                    .Where(p => p.ProductVariants.Any(v => v.IsSelling && !v.IsDeleted))
+                    .ApplyPaging(request.PageIndex, request.PageSize)
+                    .Select(p => new ProductResponseDto
                     {
-                        PublicId = v.PublicId,
-                        SizeId = v.SizeId,
-                        Size = v.Size!.Size,
-                        ColorId = v.ColorId,
-                        ColorName = v.Color != null ? v.Color.ColorName : null,
-                        Stock = v.Stock,
-                        Price = v.Price,
-                        ImageUrl = v.ImageUrl,
-                        IsSelling = v.IsSelling
+                        PublicId = p.PublicId,
+                        Brand = p.Brand ?? string.Empty,
+                        ProductName = p.ProductName,
+                        Variants = p.ProductVariants
+                            .Where(v => v.IsSelling && !v.IsDeleted)
+                            .Select(v => new ProductVariantResponseDto
+                            {
+                                PublicId = v.PublicId,
+                                SizeId = v.SizeId,
+                                Size = v.Size != null ? v.Size.Size : 0,
+                                ColorId = v.ColorId,
+                                ColorName = v.Color != null ? v.Color.ColorName : null,
+                                Stock = v.Stock,
+                                Price = v.Price,
+                                ImageUrl = v.ImageUrl,
+                                IsSelling = v.IsSelling
+                            })
+                            .ToList()
                     })
-                    .ToList()
-            })
-            .ToListAsync(token);
+                    .ToListAsync(cancel);
 
-        // Build and return paged result
-        var pageResult = new PageResult<ProductResponseDto>
-        {
-            Items = products,
-            TotalCount = totalCount,
-            PageNumber = request.PageIndex,
-            PageSize = request.PageSize
-        };
+                var pageResult = new PageResult<ProductResponseDto>
+                {
+                    Items = products,
+                    TotalCount = totalCount,
+                    PageNumber = request.PageIndex,
+                    PageSize = request.PageSize
+                };
+                return pageResult;
+            },
+            tags: [CacheTag.Product],
+            cancellationToken: token);
 
-        return pageResult;
+        return cachedListProduct;
     }
 
     public async Task<ErrorOr<ProductResponseDto>> GetProductByGuidAsync(Guid productGuid, CancellationToken token)
@@ -99,7 +109,6 @@ public class ProductService(IUnitOfWork uow, IProductRepository productRepositor
                 };
                 return productDto;
             },
-            tags: [CacheTag.Product],
             cancellationToken: token);
 
         // if in cache, return it directly without accessing the database
@@ -139,7 +148,7 @@ public class ProductService(IUnitOfWork uow, IProductRepository productRepositor
         // Add product to repository and save
         productRepository.Add(product);
         await uow.SaveChangesAsync(token);
-
+        await cache.RemoveByTagAsync(CacheTag.Product, token);
         return product.PublicId;
     }
 
@@ -172,6 +181,7 @@ public class ProductService(IUnitOfWork uow, IProductRepository productRepositor
                 existingVariant.IsSelling = variantDto.IsSelling;
                 existingVariant.ImageUrl = variantDto.ImageUrl;
                 existingVariant.Price = variantDto.Price;
+                existingVariant.UpdatedAt = DateTime.UtcNow;
             }
             else
             {
@@ -187,7 +197,6 @@ public class ProductService(IUnitOfWork uow, IProductRepository productRepositor
                     ProductId = product.Id
                 };
                 product.ProductVariants.Add(newProductVariant);
-                await uow.SaveChangesAsync(token);
             }
         }
 
@@ -200,13 +209,13 @@ public class ProductService(IUnitOfWork uow, IProductRepository productRepositor
             .ToList();
 
         // soft delete
-        foreach (var variant in variantsToRemove)
-            variant.IsDeleted = true;
+        foreach (var variant in variantsToRemove) variant.IsDeleted = true;
 
         // Update product in repository and save
         productRepository.Update(product);
         await uow.SaveChangesAsync(token);
-
+        await cache.RemoveAsync(CacheKey.GenerateProductDetailsCacheKey(product.PublicId), token);
+        await cache.RemoveByTagAsync(CacheTag.Product, token);
         return Result.Updated;
     }
 
@@ -222,7 +231,8 @@ public class ProductService(IUnitOfWork uow, IProductRepository productRepositor
         // Soft delete: mark all variants as deleted
         foreach (var variant in product.ProductVariants) variant.IsDeleted = true;
         await uow.SaveChangesAsync(token);
-
+        await cache.RemoveAsync(CacheKey.GenerateProductDetailsCacheKey(productGuid), token);
+        await cache.RemoveByTagAsync(CacheTag.Product, token);
         return Result.Deleted;
     }
 }
---
+++ Backend/src/ShoeStore.Infrastructure/Migrations/20260411060548_AddTimeVariant.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoeStore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimeVariant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_at",
+                table: "product_variants",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "updated_at",
+                table: "product_variants",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created_at",
+                table: "product_variants");
+
+            migrationBuilder.DropColumn(
+                name: "updated_at",
+                table: "product_variants");
+        }
+    }
+}
