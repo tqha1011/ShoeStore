36f76fefca3a0db3947d5dfe2b5e26ae03f6cdb2
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-04-18 23:37:58 +0700

    fix

---
+++ Backend/src/ShoeStore.Application/DTOs/VoucherDtos/CreateVoucherDto.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ShoeStore.Application.DTOs.VoucherDtos
+{
+    public class CreateVoucherDto
+    {
+        public string? VocherName { get; set; }
+        public string? VoucherDescription { get; set; }
+        public decimal Discount { get; set; }
+        public int VoucherScope { get; set; }
+        public int DiscountType { get; set; }
+        public decimal MaxPriceDiscount { get; set; }
+        public DateTime? ValidFrom { get; set; }
+        public DateTime? ValidTo { get; set; }
+        public int? MaxUsagePerUser { get; set; }
+        public int TotalQuantity { get; set; }
+        public decimal MinOrderPrice { get; set; }
+    }
+}
---
+++ Backend/src/ShoeStore.Application/DTOs/VoucherDtos/UpdateVoucherDto.cs
@@ -0,0 +1,17 @@
+﻿namespace ShoeStore.Application.DTOs.VoucherDtos
+{
+    public class UpdateVoucherDto
+    { 
+        public string? VoucherDescription { get; set; }
+        public int? VoucherScope { get; set; }
+        public int? DiscountType { get; set; }
+        public decimal MaxPriceDiscount { get; set; }
+        public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+        public DateTime? ValidFrom { get; set; }
+        public DateTime? ValidTo { get; set; }
+        public int? MaxUsagePerUser { get; set; }
+        public int? TotalQuantity { get; set; }
+        public bool? IsDeleted { get; set; } = false;
+        public decimal? MinOrderPrice { get; set; };
+    }
+}
