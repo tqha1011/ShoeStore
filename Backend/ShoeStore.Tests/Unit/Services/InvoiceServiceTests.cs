800a2a2dcbb5384249d5747e156ad78b22a8d70c
Tran Quang Ha <tranquangha2006@gmail.com>
2026-04-19 23:48:11 +0700

    testing(InvoiceService): write unit tests for UpdateStateByUser

--- Backend/ShoeStore.Tests/Unit/Services/InvoiceServiceTests.cs
+++ Backend/ShoeStore.Tests/Unit/Services/InvoiceServiceTests.cs
@@ -40,175 +40,311 @@ public class InvoiceServiceTests
             .ReturnsAsync((Invoice?)null);
 
         // stage 2: Action
+        var adminResult =
+            await _invoiceService.UpdateInvoiceStateByAdminAsync(fakeGuid, fakeRequest, CancellationToken.None);
+
+
+        // stage 3: Assert
+        Assert.True(adminResult.IsError);
+        Assert.Equal("Invoice.NotFound", adminResult.FirstError.Code);
+    }
+
+    [Theory]
+    // SePay (paymentId = 1)
+    [InlineData(1, InvoiceStatus.Cancelled, InvoiceStatus.Paid)]
+    [InlineData(1, InvoiceStatus.Cancelled, InvoiceStatus.Delivering)]
+    [InlineData(1, InvoiceStatus.Cancelled, InvoiceStatus.Pending)]
+    [InlineData(1, InvoiceStatus.Delivering, InvoiceStatus.Pending)]
+    [InlineData(1, InvoiceStatus.Paid, InvoiceStatus.Pending)]
+    // COD (paymentId = 2)
+    [InlineData(2, InvoiceStatus.Cancelled, InvoiceStatus.Paid)]
+    [InlineData(2, InvoiceStatus.Cancelled, InvoiceStatus.Delivering)]
+    [InlineData(2, InvoiceStatus.Cancelled, InvoiceStatus.Pending)]
+    [InlineData(2, InvoiceStatus.Pending, InvoiceStatus.Paid)]
+    [InlineData(2, InvoiceStatus.Delivering, InvoiceStatus.Pending)]
+    [InlineData(2, InvoiceStatus.Paid, InvoiceStatus.Pending)]
+    public async Task UpdateInvoiceStateByAdmin_WhenChangeStatusIsInvalidForOnlinePayment_ReturnForbidden(
+        int paymentId,
+        InvoiceStatus currStatus,
+        InvoiceStatus newStatus)
+    {
+        // Arrange
+        var fakeGuid = Guid.NewGuid();
+        var fakeUser = new User
+        {
+            Id = 1,
+            UserName = "testuser",
+            Password = "testpass",
+            PublicId = Guid.NewGuid(),
+            Email = "test@gmail.com"
+        };
+        var fakeRequest = new UpdateStateRequestDto
+        {
+            Status = newStatus
+        };
+
+        var fakeInvoice = new Invoice
+        {
+            Id = 1,
+            UserId = 1,
+            User = fakeUser,
+            FullName = "Test User",
+            Status = currStatus,
+            Phone = "",
+            PaymentId = paymentId,
+            ShippingAddress = "",
+            FinalPrice = 100
+        };
+
+        _mockRepo.Setup(repo => repo.GetByPublicIdAsync(fakeGuid, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(fakeInvoice);
+
+        // step 2: Action
         var result =
             await _invoiceService.UpdateInvoiceStateByAdminAsync(fakeGuid, fakeRequest, CancellationToken.None);
-        
+
+        // step 3: Assert
+        Assert.True(result.IsError);
+        Assert.Equal("Invoice.Forbidden", result.FirstError.Code);
+    }
+
+    [Theory]
+    [InlineData(1, InvoiceStatus.Pending, InvoiceStatus.Paid)]
+    public async Task UpdateInvoiceStateByAdmin_WhenChangeStatusToPaidWithoutPayment_ReturnValidation(
+        int paymentId,
+        InvoiceStatus currStatus,
+        InvoiceStatus newStatus)
+    {
+        var fakeGuid = Guid.NewGuid();
+        var fakeUser = new User
+        {
+            Id = 1,
+            UserName = "testuser",
+            Password = "testpass",
+            PublicId = Guid.NewGuid(),
+            Email = "test@gmail.com"
+        };
+        var fakeRequest = new UpdateStateRequestDto
+        {
+            Status = newStatus
+        };
+
+        List<PaymentTransaction> fakePaymentTransactions = [];
+
+        var fakeInvoice = new Invoice
+        {
+            Id = 1,
+            UserId = 1,
+            User = fakeUser,
+            FullName = "Test User",
+            Status = currStatus,
+            Phone = "",
+            PaymentId = paymentId,
+            ShippingAddress = "",
+            FinalPrice = 100,
+            PaymentTransactions = fakePaymentTransactions,
+            OrderCode = "DH123"
+        };
+
+        _mockRepo.Setup(repo => repo.GetByPublicIdAsync(fakeGuid, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(fakeInvoice);
+
+        var result =
+            await _invoiceService.UpdateInvoiceStateByAdminAsync(fakeGuid, fakeRequest, CancellationToken.None);
+
+        Assert.True(result.IsError);
+        Assert.Equal("Invoice.InvalidStatus", result.FirstError.Code);
+    }
+
+    [Theory]
+    [InlineData(1, InvoiceStatus.Pending, InvoiceStatus.Paid)]
+    public async Task UpdateInvoiceStateByAdmin_WhenChangeStatusToPaidWithNotEnoughPayment_ReturnValidation(
+        int paymentId,
+        InvoiceStatus currStatus,
+        InvoiceStatus newStatus)
+    {
+        var fakeGuid = Guid.NewGuid();
+        var fakeUser = new User
+        {
+            Id = 1,
+            UserName = "testuser",
+            Password = "testpass",
+            PublicId = Guid.NewGuid(),
+            Email = "test@gmail.com"
+        };
+        var fakeRequest = new UpdateStateRequestDto
+        {
+            Status = newStatus
+        };
+
+        List<PaymentTransaction> fakePaymentTransactions =
+        [
+            new()
+            {
+                Id = 1,
+                InvoiceId = 1,
+                Amount = 10,
+                PaymentId = paymentId,
+                OrderCode = "DH123"
+            },
+
+            new()
+            {
+                Id = 2,
+                InvoiceId = 1,
+                Amount = 30,
+                PaymentId = paymentId,
+                OrderCode = "DH123"
+            }
+        ];
+
+        var fakeInvoice = new Invoice
+        {
+            Id = 1,
+            UserId = 1,
+            User = fakeUser,
+            FullName = "Test User",
+            Status = currStatus,
+            Phone = "",
+            PaymentId = paymentId,
+            ShippingAddress = "",
+            FinalPrice = 100,
+            PaymentTransactions = fakePaymentTransactions,
+            OrderCode = "DH123"
+        };
+
+        _mockRepo.Setup(repo => repo.GetByPublicIdAsync(fakeGuid, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(fakeInvoice);
+
+        var result =
+            await _invoiceService.UpdateInvoiceStateByAdminAsync(fakeGuid, fakeRequest, CancellationToken.None);
+
+        Assert.True(result.IsError);
+        Assert.Equal("Invoice.InvalidStatus", result.FirstError.Code);
+    }
+
+    [Theory]
+    // SePay (paymentId = 1)
+    [InlineData(1, InvoiceStatus.Cancelled, InvoiceStatus.Paid)]
+    [InlineData(1, InvoiceStatus.Cancelled, InvoiceStatus.Delivering)]
+    [InlineData(1, InvoiceStatus.Cancelled, InvoiceStatus.Pending)]
+    [InlineData(1, InvoiceStatus.Delivering, InvoiceStatus.Pending)]
+    [InlineData(1, InvoiceStatus.Paid, InvoiceStatus.Pending)]
+    // COD (paymentId = 2)
+    [InlineData(2, InvoiceStatus.Cancelled, InvoiceStatus.Paid)]
+    [InlineData(2, InvoiceStatus.Cancelled, InvoiceStatus.Delivering)]
+    [InlineData(2, InvoiceStatus.Cancelled, InvoiceStatus.Pending)]
+    [InlineData(2, InvoiceStatus.Pending, InvoiceStatus.Paid)]
+    [InlineData(2, InvoiceStatus.Delivering, InvoiceStatus.Pending)]
+    [InlineData(2, InvoiceStatus.Paid, InvoiceStatus.Pending)]
+    public async Task UpdateInvoiceStateByUser_WhenChangeToInvalidStatus_ReturnValidation(
+        int paymentId,
+        InvoiceStatus currStatus,
+        InvoiceStatus newStatus)
+    {
+        var fakeGuid = Guid.NewGuid();
+        var fakeUser = new User
+        {
+            Id = 1,
+            UserName = "testuser",
+            Password = "testpass",
+            PublicId = Guid.NewGuid(),
+            Email = "test@gmail.com"
+        };
+        var fakeRequest = new UpdateStateRequestDto
+        {
+            Status = newStatus
+        };
+
+        var fakeInvoice = new Invoice
+        {
+            Id = 1,
+            UserId = 1,
+            User = fakeUser,
+            FullName = "Test User",
+            Status = currStatus,
+            Phone = "",
+            PaymentId = paymentId,
+            ShippingAddress = "",
+            FinalPrice = 100,
+            OrderCode = "DH123"
+        };
+
+        _mockRepo.Setup(repo => repo.GetByPublicIdAsync(fakeGuid, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(fakeInvoice);
+
+        var result =
+            await _invoiceService.UpdateInvoiceStateByAdminAsync(fakeGuid, fakeRequest, CancellationToken.None);
+
+        Assert.True(result.IsError);
+        Assert.Equal("Invoice.Forbidden", result.FirstError.Code);
+    }
+
+    [Fact]
+    public async Task UpdateInvoiceStateByUser_WhenInvoiceDoesNotExist_ReturnNotFound()
+    {
+        // unit test include 3 stages : Arrange, Action, Assert
+        // Stage 1: Arrange
+        var fakeGuid = Guid.NewGuid();
+        var fakeRequest = new UpdateStateRequestDto
+        {
+            Status = InvoiceStatus.Paid
+        };
+
+        // set up the invoice is not exist in database
+        _mockRepo.Setup(repo => repo.GetByPublicIdAsync(fakeGuid, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Invoice?)null);
+
+        // stage 2: Action
+        var adminResult =
+            await _invoiceService.UpdateInvoiceStateByUserAsync(fakeGuid, fakeRequest, CancellationToken.None);
+
+
         // stage 3: Assert
-        Assert.True(result.IsError);
-        Assert.Equal("Invoice.NotFound", result.FirstError.Code);
+        Assert.True(adminResult.IsError);
+        Assert.Equal("Invoice.NotFound", adminResult.FirstError.Code);
     }
 
     [Fact]
-    public async Task UpdateInvoiceStateByAdmin_WhenChangeStatusFromPaidToCancelled_ReturnForbidden()
+    public async Task UpdateInvoiceStateByUser_WhenUserIsNotOwner_ReturnUnauthorized()
     {
-        // Arrange
         var fakeGuid = Guid.NewGuid();
-        var fakeUser = new User
-        {
-            Id = 1,
-            UserName = "testuser",
-            Password = "testpass",
-            Email = "test@gmail.com",
-        };
+        var fakeId = Guid.NewGuid();
+        _currentUser.Setup(cu => cu.Id).Returns(fakeId);
         var fakeRequest = new UpdateStateRequestDto
         {
-            Status = InvoiceStatus.Paid
+            Status = InvoiceStatus.Cancelled
+        };
+
+        var fakeUser = new User
+        {
+            Id = 2,
+            UserName = "testuser2",
+            Password = "testpass2",
+            PublicId = Guid.NewGuid(),
+            Email = ""
         };
 
         var fakeInvoice = new Invoice
         {
             Id = 1,
             UserId = 1,
-            User = fakeUser,
             FullName = "Test User",
-            Status = InvoiceStatus.Cancelled,
-            Phone = "",
-            PaymentId = 1,
-            ShippingAddress = "",
-            FinalPrice = 100,
-        };
-        
-        _mockRepo.Setup(repo => repo.GetByPublicIdAsync(fakeGuid, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(fakeInvoice);
-        
-        // step 2: Action
-        var result = await _invoiceService.UpdateInvoiceStateByAdminAsync(fakeGuid, fakeRequest, CancellationToken.None);
-        
-        // step 3: Assert
-        Assert.True(result.IsError);
-        Assert.Equal("Invoice.Forbidden", result.FirstError.Code);
-    }
-    
-    [Fact]
-    public async Task UpdateInvoiceStateByAdmin_WhenChangeStatusFromCancelledToDelivering_ReturnForbidden()
-    {
-        // Arrange
-        var fakeGuid = Guid.NewGuid();
-        var fakeUser = new User
-        {
-            Id = 1,
-            UserName = "testuser",
-            Password = "testpass",
-            Email = "test@gmail.com",
-        };
-        var fakeRequest = new UpdateStateRequestDto
-        {
-            Status = InvoiceStatus.Delivering
-        };
-
-        var fakeInvoice = new Invoice
-        {
-            Id = 1,
-            UserId = 1,
             User = fakeUser,
-            FullName = "Test User",
-            Status = InvoiceStatus.Cancelled,
-            Phone = "",
-            PaymentId = 1,
-            ShippingAddress = "",
-            FinalPrice = 100,
-        };
-        
-        _mockRepo.Setup(repo => repo.GetByPublicIdAsync(fakeGuid, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(fakeInvoice);
-        
-        // step 2: Action
-        var result = await _invoiceService.UpdateInvoiceStateByAdminAsync(fakeGuid, fakeRequest, CancellationToken.None);
-        
-        // step 3: Assert
-        Assert.True(result.IsError);
-        Assert.Equal("Invoice.Forbidden", result.FirstError.Code);
-    }
-    
-    [Fact]
-    public async Task UpdateInvoiceStateByAdmin_WhenChangeStatusFromCancelledToPending_ReturnForbidden()
-    {
-        // Arrange
-        var fakeGuid = Guid.NewGuid();
-        var fakeUser = new User
-        {
-            Id = 1,
-            UserName = "testuser",
-            Password = "testpass",
-            Email = "test@gmail.com",
-        };
-        var fakeRequest = new UpdateStateRequestDto
-        {
-            Status = InvoiceStatus.Pending
-        };
-
-        var fakeInvoice = new Invoice
-        {
-            Id = 1,
-            UserId = 1,
-            User = fakeUser,
-            FullName = "Test User",
-            Status = InvoiceStatus.Cancelled,
-            Phone = "",
-            PaymentId = 1,
-            ShippingAddress = "",
-            FinalPrice = 100,
-        };
-        
-        _mockRepo.Setup(repo => repo.GetByPublicIdAsync(fakeGuid, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(fakeInvoice);
-        
-        // step 2: Action
-        var result = await _invoiceService.UpdateInvoiceStateByAdminAsync(fakeGuid, fakeRequest, CancellationToken.None);
-        
-        // step 3: Assert
-        Assert.True(result.IsError);
-        Assert.Equal("Invoice.Forbidden", result.FirstError.Code);
-    }
-    
-    [Fact]
-    public async Task UpdateInvoiceStateByAdmin_WhenChangeStatusToPaidWithoutPayment_ReturnForbidden()
-    {
-        // Arrange
-        var fakeGuid = Guid.NewGuid();
-        var fakeUser = new User
-        {
-            Id = 1,
-            UserName = "testuser",
-            Password = "testpass",
-            Email = "test@gmail.com",
-        };
-        var fakeRequest = new UpdateStateRequestDto
-        {
-            Status = InvoiceStatus.Paid
-        };
-
-        var fakeInvoice = new Invoice
-        {
-            Id = 1,
-            UserId = 1,
-            User = fakeUser,
-            FullName = "Test User",
             Status = InvoiceStatus.Pending,
             Phone = "",
             PaymentId = 1,
             ShippingAddress = "",
             FinalPrice = 100,
+            OrderCode = "DH123"
         };
-        
+
         _mockRepo.Setup(repo => repo.GetByPublicIdAsync(fakeGuid, It.IsAny<CancellationToken>()))
             .ReturnsAsync(fakeInvoice);
         
-        // step 2: Action
-        var result = await _invoiceService.UpdateInvoiceStateByAdminAsync(fakeGuid, fakeRequest, CancellationToken.None);
+        var result = await _invoiceService.UpdateInvoiceStateByUserAsync(fakeGuid, fakeRequest, CancellationToken.None);
         
-        // step 3: Assert
         Assert.True(result.IsError);
-        Assert.Equal("Invoice.Forbidden", result.FirstError.Code);
+        Assert.Equal("User.Unauthorized", result.FirstError.Code);
     }
 }
