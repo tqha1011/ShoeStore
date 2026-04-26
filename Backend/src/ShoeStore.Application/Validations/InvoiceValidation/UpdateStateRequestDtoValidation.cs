77fcb603f5e2057e5bd0f15ea3fa25964130cd8d
Tran Quang Ha <tranquangha2006@gmail.com>
2026-04-26 15:05:02 +0700

    Fix/cart item (#242)
    * feat(CartItemService): add GetUserCart , ignore query filters for better UX
    
    * fix(validation dto): remove validate status for only user

--- Backend/src/ShoeStore.Application/Validations/InvoiceValidation/UpdateStateRequestDtoValidation.cs
+++ Backend/src/ShoeStore.Application/Validations/InvoiceValidation/UpdateStateRequestDtoValidation.cs
@@ -1,6 +1,6 @@
 ﻿using FluentValidation;
 using ShoeStore.Application.DTOs.InvoiceDTOs;
-using ShoeStore.Domain.Enum;
+
 namespace ShoeStore.Application.Validations.InvoiceValidation
 {
     public class UpdateStateRequestDtoValidation : AbstractValidator<UpdateStateRequestDto>
@@ -10,11 +10,6 @@ namespace ShoeStore.Application.Validations.InvoiceValidation
             RuleFor(x => x.Status)
                 .IsInEnum()
                 .WithMessage("Invalid status value");
-
-            // Client Only Cancel
-            RuleFor(x => x.Status)
-                .Must(status => status == InvoiceStatus.Cancelled)
-                .WithMessage("You can only cancel the order");
         }
     }
 }
