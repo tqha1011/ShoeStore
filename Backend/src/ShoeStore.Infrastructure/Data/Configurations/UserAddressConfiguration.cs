57097e41aa7c450fa022805815f7d23f067452e2
Tran Quang Ha <tranquangha2006@gmail.com>
2026-05-05 17:48:00 +0700

    perf(database): configure index for UserAddress to avoid duplicated data and optimize query performance

--- Backend/src/ShoeStore.Infrastructure/Data/Configurations/UserAddressConfiguration.cs
+++ Backend/src/ShoeStore.Infrastructure/Data/Configurations/UserAddressConfiguration.cs
@@ -9,14 +9,19 @@ public class UserAddressConfiguration : IEntityTypeConfiguration<UserAddress>
     public void Configure(EntityTypeBuilder<UserAddress> builder)
     {
         builder.HasKey(addr => addr.Id);
-        
+
         builder.Property(addr => addr.Address)
             .IsRequired()
             .HasMaxLength(500);
-        
+
         builder.HasOne(addr => addr.User)
             .WithMany(u => u.UserAddresses)
             .HasForeignKey(addr => addr.UserId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(addr => new { addr.UserId, addr.Address }).IsUnique();
+        builder.HasIndex(addr => addr.UserId)
+            .IsUnique()
+            .HasFilter("is_default = true");
     }
 }
---
+++ Backend/src/ShoeStore.Infrastructure/Migrations/20260505104658_UserAddressIndex.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoeStore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserAddressIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_user_addresses_user_id",
+                table: "user_addresses");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_addresses_user_id",
+                table: "user_addresses",
+                column: "user_id",
+                unique: true,
+                filter: "is_default = true");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_addresses_user_id_address",
+                table: "user_addresses",
+                columns: new[] { "user_id", "address" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_user_addresses_user_id",
+                table: "user_addresses");
+
+            migrationBuilder.DropIndex(
+                name: "ix_user_addresses_user_id_address",
+                table: "user_addresses");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_addresses_user_id",
+                table: "user_addresses",
+                column: "user_id");
+        }
+    }
+}
