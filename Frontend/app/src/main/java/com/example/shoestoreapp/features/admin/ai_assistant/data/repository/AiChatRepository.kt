1d8226fc9717cdd853a2a2a9c8b95ca078e3e4fb
Nguyễn Lê Hoàng Hảo <nlhhao888@gmail.com>
2026-05-08 23:31:47 +0700

    feat(ai_assistant): implement AiChatRepository with SSE streaming support
    - Add baseStreamCall using OkHttp to handle text/event-stream
    
    - Integrate ChatSessionApi for session management
    
    - Setup streamChatStatistics and streamGenerateCampaign methods

---
+++ Frontend/app/src/main/java/com/example/shoestoreapp/features/admin/ai_assistant/data/repository/AiChatRepository.kt
@@ -0,0 +1,55 @@
+package com.example.shoestoreapp.features.admin.ai_assistant.data.repository
+
+import com.example.shoestoreapp.core.utils.Constants
+import com.example.shoestoreapp.features.admin.ai_assistant.data.remote.ChatSessionApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class AiChatRepository (
+    private val sessionApi : ChatSessionApi,
+    private val okHttpClient: OkHttpClient
+) {
+    suspend fun createSession() = sessionApi.createSession()
+    suspend fun getSessions(pageNumber : Int, pageSize : Int) = sessionApi.getSession(pageNumber, pageSize)
+
+    fun streamChatStatistics(sessionId : String, title : String) =
+        baseStreamCall("${Constants.BASE_URL}/api/chatbot/chat-statistics?publicSessionId=$sessionId", title)
+
+    fun streamGenerateCampaign(sessionId : String, content : String) =
+        baseStreamCall("${Constants.BASE_URL}/api/chatbot/generate-campaign", content, sessionId)
+
+    private fun baseStreamCall(url : String, content : String, sessionId : String? = null) : Flow<String> = flow {
+        val json = JSONObject().apply {
+            put("Content", content)
+            if (sessionId != null) put("PublicSessionId", sessionId)
+        }.toString()
+
+        val body = json.toRequestBody("application/json".toMediaType())
+        val request = Request.Builder()
+            .url(url)
+            .post(body)
+            .addHeader("Accept", "text/event-stream")
+            .build()
+
+        okHttpClient.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw Exception("Stream API Error: ${response.code}")
+
+            val reader = BufferedReader(InputStreamReader(response.body!!.byteStream()))
+            var line : String?
+            while (reader.readLine().also { line = it } != null) {
+               if ( line!!.startsWith("data: ")){
+                   val chunk = line!!.substring(6)
+                   if (chunk.trim() == "[DONE]") break
+                   emit(chunk)
+               }
+            }
+        }
+    }
+}
