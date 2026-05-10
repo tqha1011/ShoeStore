ad6904de6c4a7a6bc32464af90c117ed10a4ac57
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-05-10 23:17:21 +0700

    feat: validation for address dto

--- Backend/src/ShoeStore.Api/Controllers/AddressController.cs
+++ Backend/src/ShoeStore.Api/Controllers/AddressController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShoeStore.Application.DTOs.AddressDTOs;
+using ShoeStore.Application.Interface.AddressInterface;
 
 namespace ShoeStore.Api.Controllers
 {
-    public class AddressController : Controller
+    [ApiController]
+    [Route("api/address")]
+    [Authorize]
+    public class AddressController(IAddressService addressService) : ControllerBase
     {
-        public IActionResult Index()
+        [HttpPost("{userGuid}")]
+        public async Task<IActionResult> CreateAddress(Guid userGuid, [FromBody] CreateAddressDto dto, CancellationToken token)
         {
-            return View();
+            var result = await addressService.CreateAddressAsync(userGuid, dto, token);
+
+            return Ok(result);
         }
     }
 }
